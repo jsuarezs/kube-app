b1b2c6bc5a40539c7afb38bb90ce2060480aedc8
Javier Suarez <javiersuarezsanz86@gmail.com>
2021-02-14 20:36:33 +0100

    Removing JSON logs as it's failing

--- app.py
+++ app.py
@@ -3,8 +3,19 @@ import time
 import redis
 from flask import Flask
 
+import os
+
+# Set environment variables
+REDIS_HOST = os.environ['REDIS_HOST']
+REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
+
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+
+
+cache = redis.Redis(host=REDIS_HOST, port=6379, password=REDIS_PASSWORD)
+
+#Removing literal setting and change to env values
+#cache = redis.Redis(host='redis', port=6379)
 
 def get_hit_count():
     retries = 5
