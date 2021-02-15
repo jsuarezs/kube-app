c29a7cfb3e7d6804c0979f9b5b51dbb9791ae3fa
Javier Suarez <javiersuarezsanz86@gmail.com>
2021-02-15 15:07:09 +0100

    Adding json-logging

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import time
 import redis
 from flask import Flask
 
+import json_logging, logging, sys
 import os
 
 # Set environment variables
@@ -10,6 +11,13 @@ REDIS_HOST = os.environ['REDIS_HOST']
 REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
 
 app = Flask(__name__)
+
+# intialize logger
+json_logging.init_flask(enable_json=True)
+json_logging.init_request_instrument(app)
+logger = logging.getLogger("flask-counter")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))    
 
 
 cache = redis.Redis(host=REDIS_HOST, port=6379, password=REDIS_PASSWORD)
@@ -32,3 +40,11 @@ def get_hit_count():
 def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
+
+@app.route('/health/live')
+def health_live():
+    return "Ok"
+
+@app.route('/health/ready')
+def health_ready():
+    return "Ok"
