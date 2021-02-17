7aed6094d6b63a3229b7af6782891d5a20671df4
Javier Suarez <javiersuarezsanz86@gmail.com>
2021-02-17 13:29:30 +0100

    Code modified to integrate Prometheus server

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import redis
 from flask import Flask
 
 import json_logging, logging, sys
+
+from prometheus_client import start_http_server, Counter
+
+
 import os
 
 # Set environment variables
@@ -17,29 +21,38 @@ json_logging.init_flask(enable_json=True)
 json_logging.init_request_instrument(app)
 logger = logging.getLogger("flask-counter")
 logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler(sys.stdout))    
-
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 cache = redis.Redis(host=REDIS_HOST, port=6379, password=REDIS_PASSWORD)
 
-#Removing literal setting and change to env values
-#cache = redis.Redis(host='redis', port=6379)
+start_http_server(8000)
+
+c = Counter("redis_calls", "Number of calls to redis", ["status"])
+
 
 def get_hit_count():
     retries = 5
     while True:
         try:
-            return cache.incr('hits')
+            hits = cache.incr('hits')
+            c.labels(status="success").inc()
+            return hits
         except redis.exceptions.ConnectionError as exc:
             if retries == 0:
+                c.labels(status="failure").inc()
                 raise exc
             retries -= 1
             time.sleep(0.5)
 
+
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    message = 'Hello Keepcoding! I have been seen {} times.\n'.format(count)
+    with open("log.txt","a+") as fo:
+        fo.write(message)
+    
+    return message
 
 @app.route('/health/live')
 def health_live():
